a9c1f8bdbeddba5db5d9fa8daf416b40c84202b4
Ghazan Usman <meetghz@outlook.com>
2018-12-24 10:17:44 -0800

    first one

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Ghazan\PycharmProjects\portfolio-project\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
