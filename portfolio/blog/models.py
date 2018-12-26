312f67cf4ef4a35200faf44ec7842feb29d67bfa
Ghazan Usman <meetghz@outlook.com>
2018-12-26 04:40:23 -0800

    Blog Model added up and running

--- portfolio/blog/models.py
+++ portfolio/blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+class blog(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField()
+    body = models.TextField();
+    image = models.ImageField(upload_to='images/')
 
-# Create your models here.
+
+
